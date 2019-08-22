6d17123cc693c4a11b9874b86ba7c3e024346a02
Tom Ekander <ekander.tom@gmail.com>
2019-08-22 17:46:08 +0200

    chore: change order of sections

--- doczrc.js
+++ doczrc.js
@@ -1,5 +1,7 @@
 export default {
-  title: 'Egendata Documentation',
-  dest: '/docs',
-  base: process.env.DOCZ_BASE || '/'
-}
+  menu: ["General", "Architecture"],
+  title: "Egendata Documentation",
+  dest: "/docs",
+  base: process.env.DOCZ_BASE || "/"
+};
+
